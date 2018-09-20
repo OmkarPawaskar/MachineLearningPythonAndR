d37e86131fe6e3d8836bbffa110301bd984b5d8f
OmkarPawaskar <omkarpawaskar96@hotmail.com>
2018-09-21 01:07:25 +0530

    Update decision_tree_classification.R

--- 3.Classification/6.Decision Tree Classification/decision_tree_classification.R
+++ 3.Classification/6.Decision Tree Classification/decision_tree_classification.R
@@ -66,3 +66,7 @@ plot(set[, -3], main = 'Decision Tree Classification (Test set)',
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+#Plotting the Decision Tree
+plot(classifier)
+text(classifier)
